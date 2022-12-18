2e9cfe7bf4384b3981744533c5b081d2a4d2b06a
lyazj <seeson@pku.edu.cn>
2022-12-18 16:02:43 +0800

    Add multiprocess support.

--- delphes/Collect.C
+++ delphes/Collect.C
@@ -1,12 +1,34 @@
 #include <external/ExRootAnalysis/ExRootTreeReader.h>
 #include <classes/DelphesClasses.h>
 
+// for memory sharing and process control
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <err.h>
+
 vector<string> directories = {
   "../1000/mumuEE3D",
   "../1000/mumuEE10D",
+  "../1000/mumuNN3D",
+  "../1000/mumuNN10D",
   "../bg/mumutt3D",
   "../bg/mumutt10D",
+  "../bg/mumubb3D",
+  "../bg/mumubb10D",
 };
+atomic_size_t *directory_index;
+
+size_t ncpu()
+{
+  size_t n;
+  FILE *p = popen("cat /proc/cpuinfo | grep processor | wc -l", "r");
+  if(fscanf(p, "%zu", &n) != 1)
+    throw runtime_error("error getting processor number");
+  pclose(p);
+  return n;
+}
 
 bool split(string path, string &dir, string &base)
 {
@@ -90,95 +112,115 @@ bool near(const TLorentzVector &v1, const TLorentzVector &v2)
   return hypot(v1.Eta() - v2.Eta(), v1.Phi() - v2.Phi()) < 0.5;
 }
 
-void Collect(const vector<string> &directories = ::directories)
+void Collect(const string &directory)
 {
-  // gSystem->Load("libDelphes.so");
+  string cme, mass;
+  string label = get_label(directory, &cme, NULL, &mass);
 
-  for(const string &directory : directories)
+  TChain *Delphes = new TChain("Delphes");
+  vector<string> rootfiles = get_rootfiles(directory);
+  for(string rootfile : rootfiles)
+    Delphes->Add(rootfile.c_str());
+
+  ExRootTreeReader *reader = new ExRootTreeReader(Delphes);
+  TClonesArray *BrElectron = reader->UseBranch("Electron");
+  TClonesArray *BrMuon = reader->UseBranch("Muon");
+  TClonesArray *VLCjetR05 = reader->UseBranch("VLCjetR05_inclusive");
+  Long64_t entries = reader->GetEntries();
+
+  TFile *outfile = new TFile((label + "_collect_" + mass + "_" + cme + ".root").c_str(), "recreate");
+  TTree *Collect = new TTree("Collect", "Collected features for analysis");
+  TClonesArray *BrJet = new TClonesArray("Jet", 10);
+  Collect->Branch("Jet", &BrJet);
+
+  for(Long64_t entry = 0; entry < entries; ++entry)
   {
-    clog << "Directory: " << directory << endl;
+    reader->ReadEntry(entry);
 
-    string cme, mass;
-    string label = get_label(directory, &cme, NULL, &mass);
-    TChain *Delphes = new TChain("Delphes");
-    vector<string> rootfiles = get_rootfiles(directory);
-    for(string rootfile : rootfiles)
+    Long64_t njet = VLCjetR05->GetEntries();
+    Long64_t c = 0;
+
+    for(Long64_t i = 0; i < njet; ++i)
     {
-      // clog << "Loading: " << rootfile << endl;
-      Delphes->Add(rootfile.c_str());
-    }
-
-    ExRootTreeReader *reader = new ExRootTreeReader(Delphes);
-    TClonesArray *BrElectron = reader->UseBranch("Electron");
-    TClonesArray *BrMuon = reader->UseBranch("Muon");
-    TClonesArray *VLCjetR05 = reader->UseBranch("VLCjetR05_inclusive");
-    Long64_t entries = reader->GetEntries();
-
-    TFile *outfile = new TFile((label + "-collect-" + mass + "-" + cme + ".root").c_str(), "recreate");
-    TTree *Collect = new TTree("Collect", "Collected features for analysis");
-    TClonesArray *BrJet = new TClonesArray("Jet", 10);
-    Collect->Branch("Jet", &BrJet);
-
-    for(Long64_t entry = 0; entry < entries; ++entry)
-    {
-      reader->ReadEntry(entry);
-
-      Long64_t njet = VLCjetR05->GetEntries();
-      Long64_t c = 0;
-
-      for(Long64_t i = 0; i < njet; ++i)
+      Jet *jet = (Jet *)VLCjetR05->At(i);
+      if(jet->BTag)  // TODO check why BTag out of 0 and 1
       {
-        Jet *jet = (Jet *)VLCjetR05->At(i);
-        if(jet->BTag)  // TODO check why BTag out of 0 and 1
-        {
-          bool valid = 1;
-          TLorentzVector pj = jet->P4();
+        bool valid = 1;
+        TLorentzVector pj = jet->P4();
 
-          Long64_t nelectron = BrElectron->GetEntries();
-          for(Long64_t j = 0; j < nelectron; ++j)
+        Long64_t nelectron = BrElectron->GetEntries();
+        for(Long64_t j = 0; j < nelectron; ++j)
+        {
+          Electron *electron = (Electron *)BrElectron->At(j);
+          if(near(pj, electron->P4()))
           {
-            Electron *electron = (Electron *)BrElectron->At(j);
-            if(near(pj, electron->P4()))
+            valid = 0;
+            break;
+          }
+        }
+
+        if(valid)
+        {
+          Long64_t nmuon = BrMuon->GetEntries();
+          for(Long64_t j = 0; j < nmuon; ++j)
+          {
+            Muon *muon = (Muon *)BrMuon->At(j);
+            if(near(pj, muon->P4()))
             {
               valid = 0;
               break;
             }
           }
-
-          if(valid)
-          {
-            Long64_t nmuon = BrMuon->GetEntries();
-            for(Long64_t j = 0; j < nmuon; ++j)
-            {
-              Muon *muon = (Muon *)BrMuon->At(j);
-              if(near(pj, muon->P4()))
-              {
-                valid = 0;
-                break;
-              }
-            }
-          }
-
-          if(valid)
-            new((*BrJet)[c++]) Jet(*jet);
         }
+
+        if(valid)
+          new((*BrJet)[c++]) Jet(*jet);
       }
-      Collect->Fill();
-      BrJet->Clear();
-
-      if((entry + 1) % 1000 == 0)
-        clog << "Processing: "
-          << setw(to_string(entries).length()) << (entry + 1)
-          << " / " << entries << " entries processed" << endl;
     }
+    Collect->Fill();
+    BrJet->Clear();
 
-    // Collect->Print();
-    Collect->Write();
-
-    delete BrJet;
-    delete Collect;
-    delete outfile;
-    delete reader;
-    delete Delphes;
+    if((entry + 1) % 1000 == 0)
+      clog << "Processing " << directory << ": "
+        << setw(to_string(entries).length()) << (entry + 1)
+        << " / " << entries << " entries processed\n";
   }
+
+  // Collect->Print();
+  Collect->Write();
+
+  delete BrJet;
+  delete Collect;
+  delete outfile;
+  delete reader;
+  delete Delphes;
+}
+
+void Collect(const vector<string> &directories = ::directories)
+{
+  directory_index = (atomic_size_t *)mmap(NULL, sizeof *directory_index,
+      PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+  if(directory_index == MAP_FAILED)
+    err(1, "mmap");
+
+  size_t nworker = min(ncpu(), directories.size());
+  for(size_t w = 0; w < nworker; ++w)
+  {
+    pid_t pid = fork();
+    if(pid < 0)
+      err(1, "fork");
+    if(pid == 0)  // child
+    {
+      size_t i;
+      while((i = (*directory_index)++) < directories.size())
+        Collect(directories[i]);
+      exit(0);
+    }
+  }
+
+  for(size_t w = 0; w < nworker; ++w)
+    if(wait(NULL) < 0)
+      err(1, "wait");
+  if(munmap(directory_index, sizeof *directory_index) < 0)
+    err(1, "munmap");
 }
