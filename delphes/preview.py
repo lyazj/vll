b7cda7c28750ac1be84ac3a64253cfc206644a1c
lyazj <seeson@pku.edu.cn>
2022-12-20 21:39:27 +0800

    Add preview.

--- delphes/preview.py
+++ delphes/preview.py
@@ -5,6 +5,8 @@ import uproot
 Collect = uproot.open('mumuEE_collect_1000_3.root')['Collect']
 HepMCWeight = Collect['HepMCWeight'].array(library='np')
 Weight = Collect['Weight'].array(library='np')
+JetBTag = Collect['JetBTag'].array(library='np')
+JetTauTag = Collect['JetTauTag'].array(library='np')
 
 print('Collect.keys()', Collect.keys(), sep='\n')
 print()
@@ -13,3 +15,7 @@ print()
 print('Weight', Weight, sep='\n')
 print()
 print('(Weight[:,0] == HepMCWeight).all()', (Weight[:,0] == HepMCWeight).all(), sep='\n')
+print()
+print('JetBTag', JetBTag, sep='\n')
+print()
+print('JetTauTag', JetTauTag, sep='\n')
